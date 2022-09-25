027aa1dcaa24caee776ec38d54a9b2d5ca8584ea
AnjKura <108234350+AnjKura@users.noreply.github.com>
2022-09-24 23:02:26 -0400

    Update test_app.py

--- test_app.py
+++ test_app.py
@@ -8,3 +8,12 @@ from application import app
 def test_home_page():
     response = app.test_client().get('/')
     assert response.status_code == 200
+    
+# def test_fast():
+#   b = "Bobby"
+#   new_greeting = greet(b)
+#   assert new_greeting == "Good Evening Bobby"
+
+def test_home_page():
+    response = app.test_client().get('/')
+    assert response.status_code == 200
